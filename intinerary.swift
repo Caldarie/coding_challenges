414ecd4c2b5561bf8e509c21153cc70167abbebd
Michael Nguyen <42107315+Caldarie@users.noreply.github.com>
2021-02-27 22:47:37 +0900

    Fixed error where extending list wont update

--- intinerary.swift
+++ intinerary.swift
@@ -1,13 +1,13 @@
 //
 //  main.swift
-
+//  Oplus
 //
 //  Created by Michael Nguyen on 2021/02/27.
 //
 
 import Foundation
 
-let boardTicket = [["LAX", "HNL"], ["SFO", "LAX"], ["HNL", "NRT"]]
+let boardTicket = [["RED", "BLU"], ["LAX", "HNL"], ["SFO", "LAX"], ["NRT", "RED"], ["HNL", "NRT"]]
 
 func getIntinerary(_ dest: [[String]]) -> [String]{
     
@@ -18,12 +18,12 @@ func getIntinerary(_ dest: [[String]]) -> [String]{
     var left = 0
     var right = remainingDest.count - 1
     
-  
+    
     while (right >= 0){
         
         //check if desination has SFO
         if(remainingDest[left].contains(intialDestination)){
-            
+            print("a")
             //iterates the inner array and records the last string to check duplicates
             for (index, destination) in remainingDest[left].enumerated(){
                 result.append(destination)
@@ -33,41 +33,40 @@ func getIntinerary(_ dest: [[String]]) -> [String]{
             }
             //Removes the destination from the array
             remainingDest.remove(at: left)
-            left = 0 //resets index
+            left = -1 //resets index
+            right -= 1
         }
-
         //check if remaining destinations contain last string
-        if(remainingDest[left].contains(lastString)){
-
+        else if(remainingDest[left].contains(lastString)){
+            print("b")
             //iterates the inner array, remove duplicate and records the last string
             for (index, destination) in remainingDest[left].enumerated(){
                 if (destination != lastString){
                     result.append(destination)
-                    left = 0 //resets index
                 }
                 //if last index, updates last string
                 if index == remainingDest[left].endIndex-1{
                     lastString = destination
                 }
             }
+            
+            //Removes the destination from the array
+            remainingDest.remove(at: left)
+            left = -1 //resets index
+            right -= 1
+            
+        }else{
+            print("c")
+            
         }
         
         //adjust size of array, and increment left pointer
-        right -= 1
+        
         left += 1
-     
+        
     }
     
     return result
 }
 
 print(getIntinerary(boardTicket))
-
-
-
-
-
-
-
-
-
