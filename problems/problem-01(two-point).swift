6d69a5ff3bf1be234604a5a60a8c9c0b8c7db38d
Michael Nguyen <42107315+Caldarie@users.noreply.github.com>
2021-03-11 20:32:30 +0900

    Update problem-01(two-point).swift

--- problems/problem-01(two-point).swift
+++ problems/problem-01(two-point).swift
@@ -1,9 +1,12 @@
-//
-//  main.swift
-//  Oplus
-//
-//  Created by Michael Nguyen on 2021/02/27.
-//
+//Feedback 
+
+// Big - O Reasoning - time complextiy
+// 1. Running through the entire array to find the initialDestination, one by one. (Worstcase O(n))
+// 2. After finding the initial destination, resets left to -1 (start of array) and call remove( O(n) apparently) and update destination.
+// 3. Running through the entire array to find destination.  (O(n-1))
+// 4. after finding destination, reset left to -1 and then update destination to the next destination. then call remove (O(n-1) )
+
+// Another thing is that this code seems to assume that the entire list must be used, since right is not updated unless .contains(initialDestination)   or with lastString is actually found.
 
 import Foundation
 
@@ -29,37 +32,44 @@ func getIntineraryTwoPoint(boardTicket: [[String]]) -> [String]{
                     lastString = destination
                 }
             }
-            //Removes the destination from the array
+            
+            //resets left index
+            left = -1
+            
+            //Removes the destination from the array and resize right index
             remainingDest.remove(at: left)
-            left = -1 //resets index
-            right -= 1
+            right -= 1 
         }
+        
         //check if remaining destinations contain last string
         else if(remainingDest[left].contains(lastString)){
             print("b")
+            
             //iterates the inner array, remove duplicate and records the last string
             for (index, destination) in remainingDest[left].enumerated(){
                 if (destination != lastString){
                     result.append(destination)
                 }
+                
                 //if last index, updates last string
                 if index == remainingDest[left].endIndex-1{
                     lastString = destination
                 }
             }
             
-            //Removes the destination from the array
+            //resets left index
+            left = -1
+            
+            //Removes the destination from the array and resize right index
             remainingDest.remove(at: left)
-            left = -1 //resets index
-            right -= 1
+            right -= 1 
             
         }else{
             print("c")
             
         }
         
-        //adjust size of array, and increment left pointer
-        
+        //iterate the index until the value matches the last string
         left += 1
         
     }
